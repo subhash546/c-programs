7264a856bf02884411ce0951b955bac43c0559e8
SubhashRowtu <78291367+subhash546@users.noreply.github.com>
2021-05-18 08:07:05 +0530

    Add files via upload

---
+++ fcat.cpp
@@ -0,0 +1,19 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter a number : "<<endl;
+    cin>>n;//5
+    int fact=1;
+    for(int i=1;i<=n;i++)//2
+    {
+        fact=fact*i;//2*1
+    }
+    cout<<fact;
+    return 0;
+}
+
+
+
