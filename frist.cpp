39f451552233f8196a259a047f88bd1a106e1681
SubhashRowtu <78291367+subhash546@users.noreply.github.com>
2021-05-21 11:44:43 +0530

    Add files via upload

---
+++ frist.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter a number:"<<endl;
+    cin>>n;
+    cout<<n<<endl;
+    cout<<"welcome to c++ programing"<<endl;
+    return 0;
+
+}
+
